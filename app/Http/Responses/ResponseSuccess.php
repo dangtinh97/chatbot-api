0cdc7cf2bc52ed93c7c2dd4f46d2b6e98786932d
Dang Tinh <>
2022-05-12 11:33:02 +0700

    update

---
+++ app/Http/Responses/ResponseSuccess.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Responses;
+
+class ResponseSuccess extends ApiResponse
+{
+    public function __construct($response = [], $status = 200, $message = null)
+    {
+        $this->code = $status;
+        $this->success = true;
+        $this->message = !is_null($message) ? $message : trans('response_status.200');
+        $this->response = $response;
+    }
+}
